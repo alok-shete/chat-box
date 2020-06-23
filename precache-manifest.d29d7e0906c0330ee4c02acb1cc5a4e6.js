d23b9b7ae4956ed1ef30b4dcd6ba499beffdc9eb
alok-shete <shetealok@gmail.com>
2020-06-23 12:09:44 +0530

    Updates

---
+++ precache-manifest.d29d7e0906c0330ee4c02acb1cc5a4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45389833da35345d69e607f8be705df0",
+    "url": "/chat-box/index.html"
+  },
+  {
+    "revision": "18bd1bfaf1fe9b3f6233",
+    "url": "/chat-box/static/css/main.12cb4b3c.chunk.css"
+  },
+  {
+    "revision": "302e8a8f2da13f854ae0",
+    "url": "/chat-box/static/js/2.21692475.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/chat-box/static/js/2.21692475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bd1bfaf1fe9b3f6233",
+    "url": "/chat-box/static/js/main.7fecd8fe.chunk.js"
+  },
+  {
+    "revision": "45bf65c27f8b207760e0",
+    "url": "/chat-box/static/js/runtime-main.b1d56c2a.js"
+  }
+]);
