aadce0d2d35e9c6ca890e744c44d94c7fe18c51e
Alok shete <53371864+alok-shete@users.noreply.github.com>
2020-06-23 20:06:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyArXpOHoxD74IrHGIWRt1gMuynYoXo7Xng",
-  authDomain: "chat-box-68581.firebaseapp.com",
-  databaseURL: "https://chat-box-68581.firebaseio.com",
-  projectId: "chat-box-68581",
-  storageBucket: "chat-box-68581.appspot.com",
-  messagingSenderId: "147318437927",
-  appId: "1:147318437927:web:4b2988ab687f4f4556f602",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 firebase.initializeApp(config);
